5f4912dcf5497d1fde71cdd2ae554bef8653f1eb
joseedet <joseedet@gmail.com>
2026-01-09 04:09:21 +0100

    Reorganizar estructura del proyecto y añadir BlogAdmin

---
+++ BlogAdmin/Program.cs
@@ -0,0 +1,37 @@
+using BlogAdmin.Components;
+using BlogAdmin.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddSingleton<AuthService>();
+builder.Services.AddScoped<NotificacionesHubClient>();
+builder.Services.AddSingleton<ToastService>();
+
+builder.Services.AddHttpClient(
+    "api",
+    client =>
+    {
+        client.BaseAddress = new Uri("https://localhost:7020");
+    }
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
