cc0e89feed2ce768b791ac05c97cd68519ff5730
joseedet <joseedet@gmail.com>
2026-01-17 12:56:31 +0100

    Nuevo  NotificacionServiceTestBase

--- BlogApi.Tests/Common/NotificacionServiceTestBase.cs
+++ BlogApi.Tests/Common/NotificacionServiceTestBase.cs
@@ -1,8 +1,31 @@
-using System;
+using BlogApi.Data;
+using BlogApi.Hubs;
+using BlogApi.Repositories;
+using BlogApi.Services.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 
-namespace BlogApi.Tests.Services.Notifications;
+namespace BlogApi.Tests.Common;
 
-public class NotificacionServiceTestBase
+public abstract class NotificacionServiceTestBase
 {
+    protected readonly Mock<INotificacionRepository> Repo = new();
+    protected readonly Mock<IHubContext<NotificacionesHub>> Hub = new();
 
+    protected BlogDbContext CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<BlogDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new BlogDbContext(options);
+    }
+
+    protected NotificacionService CreateService()
+    {
+        var db = CreateDbContext();
+
+        return new NotificacionService(db, Repo.Object, Hub.Object);
+    }
 }
