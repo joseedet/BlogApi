5e917ee9b2a5f63dabe40776a184b0eb60da4289
joseedet <joseedet@gmail.com>
2026-01-19 13:55:11 +0100

    Nuevo TipoBanner

---
+++ BlogApi/Utils/TipoBanner.cs
@@ -0,0 +1,32 @@
+namespace BlogApi.Utils;
+
+/// <summary>
+/// Enum Tipo de Banner
+/// </summary>
+public enum TipoBanner
+{
+    /// <summary>
+    /// Slider
+    /// </summary>
+    Slider = 1,
+
+    /// <summary>
+    /// Hero
+    /// </summary>
+    Hero = 2,
+
+    /// <summary>
+    /// Lateral
+    /// </summary>
+    Lateral = 3,
+
+    /// <summary>
+    /// Popup
+    /// </summary>
+    Popup = 4,
+
+    /// <summary>
+    /// Anuncio
+    /// </summary>
+    Anuncio = 5,
+}
