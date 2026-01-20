a19c6f2ed3761ee93641061f718b81c24352ee5c
joseedet <joseedet@gmail.com>
2026-01-20 15:56:44 +0100

    Nuevoi: EmailLogs

---
+++ BlogApi/Controllers/EmailLogsController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers;
+
+/// <summary>
+/// Controlador de EmailLogs
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+[Authorize(Roles = "Admin")]
+public class EmailLogsController : ControllerBase
+{
+    private readonly IEmailLogService _service;
+
+    /// <summary>
+    /// Constructor de EmailLogs
+    /// </summary>
+    /// <param name="service"></param>
+    public EmailLogsController(IEmailLogService service)
+    {
+        _service = service;
+    }
+
+    /// <summary>
+    /// Obtenemos los ultimos logs
+    /// </summary>
+    /// <param name="cantidad"></param>
+    /// <returns></returns>
+    [HttpGet("{cantidad:int}")]
+    public async Task<IActionResult> Obtener(int cantidad = 50)
+    {
+        var logs = await _service.ObtenerUltimosAsync(cantidad);
+        return Ok(logs);
+    }
+}
