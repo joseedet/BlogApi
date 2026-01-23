7c1e448fb26ffff9bdc2423ecc163b675ce007d3
joseedet <joseedet@gmail.com>
2026-01-23 20:21:45 +0100

    Nuevo RefreshTokenDto

---
+++ BlogApi/DTO/RefreshTokenDto.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.DTO;
+
+public class RefreshTokenDto
+{
+    public string RefreshToken { get; set; } = string.Empty;
+}
