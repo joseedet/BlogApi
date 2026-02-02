d53d888859ad24bc67c5a6912b207cfb87340edf
joseedet <joseedet@gmail.com>
2026-02-02 19:16:12 +0100

    StatsContoller

--- BlogApi/Controllers/StatsController.cs
+++ BlogApi/Controllers/StatsController.cs
@@ -4,17 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApi.Controllers
 {
+    /// <summary>
+    /// Controlador de estadísticas
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class StatsController : ControllerBase
     {
         private readonly IStatsService _statsService;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="statsService"></param>
         public StatsController(IStatsService statsService)
         {
             _statsService = statsService;
         }
 
+        /// <summary>
+        /// Obtener estadísticas
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> GetStats()
         {
@@ -22,6 +33,11 @@ namespace BlogApi.Controllers
             return Ok(stats);
         }
 
+        /// <summary>
+        /// Actividad reciente
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <returns></returns>
         [HttpGet("actividad-reciente")]
         public async Task<IActionResult> GetActividadReciente([FromQuery] int limit = 10)
         {
