bc57f3004d6ac06bf0b2333b58ec668ed775f20f
joseedet <joseedet@gmail.com>
2026-02-04 13:30:48 +0100

    Añadir clase Dto PageDto con propiedades para la gestión de páginas

---
+++ BlogApi/DTO/PageDto.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace BlogApi.DTO;
+
+/// <summary>
+/// Dto Page
+/// </summary>
+public class PageDto
+{
+
+    /// <summary>
+    /// Identificador
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Título
+    /// </summary>
+    public string Titulo { get; set; }
+
+    /// <summary>
+    /// Slug
+    /// </summary>
+    public string Slug { get; set; }
+
+    /// <summary>
+    /// Contenido
+    /// </summary>
+    public string Contenido { get; set; }
+
+    /// <summary>
+    /// Publicado
+    /// </summary>
+    public bool Publicado { get; set; }
+
+    /// <summary>
+    /// Fecha de creación
+    /// </summary>
+    public DateTime Creado { get; set; }
+
+    /// <summary>
+    /// Fecha de actualización
+    /// </summary>
+    public DateTime Actualizado { get; set; }
+}
