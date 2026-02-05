5bacb78cd0b1f21c4b19e385fec8a74522f60134
joseedet <joseedet@gmail.com>
2026-02-05 16:39:05 +0100

    migracion

---
+++ BlogApi/Migrations/20260205152939_ADDEmailVerificationTokenReenviosTokenHash.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADDEmailVerificationTokenReenviosTokenHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmailVerificationTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpiraEn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Usado = table.Column<bool>(type: "bit", nullable: false),
+                    UsadoEn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IpCreacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserAgentCreacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IpUso = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserAgentUso = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreadoEn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Reenvios = table.Column<int>(type: "int", nullable: false),
+                    TokenHash = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailVerificationTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailVerificationTokens_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailVerificationTokens_UsuarioId",
+                table: "EmailVerificationTokens",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailVerificationTokens");
+        }
+    }
+}
